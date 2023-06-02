d21a19b7c8b6877524bce9a41d719e503aed8f0e
Ilmari <ilmari.muhonen@gmail.com>
2023-06-02 22:28:52 +0300

    testi123

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 import csv
-import tkinter as tk
-from tkinter import messagebox
 
 tapahtumat = []
 
 def lisää_tapahtuma():
-    nimi = nimi_input.get()
-    päivämäärä = päivämäärä_input.get()
-    määrä = määrä_input.get()
+    nimi = input("Syötä tapahtuman nimi: ")
+    päivämäärä = input("Syötä tapahtuman päivämäärä (VVVV-KK-PP): ")
+    määrä = float(input("Syötä tapahtuman määrä: "))
+    tapahtumat.append({"nimi": nimi, "päivämäärä": päivämäärä, "määrä": määrä})
 
-    if nimi and päivämäärä and määrä:
-        tapahtumat.append({"nimi": nimi, "päivämäärä": päivämäärä, "määrä": määrä})
-        messagebox.showinfo("Lisää tapahtuma", "Tapahtuma lisätty onnistuneesti!")
-        nimi_input.delete(0, tk.END)
-        päivämäärä_input.delete(0, tk.END)
-        määrä_input.delete(0, tk.END)
-    else:
-        messagebox.showerror("Virhe", "Täytä kaikki kentät.")
+def laske_saldo():
+    saldo = 0
+    for tapahtuma in tapahtumat:
+        saldo += tapahtuma["määrä"]
+    return saldo
 
 def näytä_tapahtumat():
-    tapahtumat_text.delete("1.0", tk.END)
     for tapahtuma in tapahtumat:
-        tapahtumat_text.insert(tk.END, f"{tapahtuma['päivämäärä']} - {tapahtuma['nimi']}: {tapahtuma['määrä']}\n")
+        print(f"{tapahtuma['päivämäärä']} - {tapahtuma['nimi']}: {tapahtuma['määrä']}")
 
-def hae_tapahtumat():
-    haku_tyyppi = haku_tyyppi_valinta.get()
-    haku_kysely = haku_kysely_input.get()
-
-    tapahtumat_text.delete("1.0", tk.END)
-
-    for tapahtuma in tapahtumat:
-        if tapahtuma[haku_tyyppi] == haku_kysely:
-            tapahtumat_text.insert(tk.END, f"{tapahtuma['päivämäärä']} - {tapahtuma['nimi']}: {tapahtuma['määrä']}\n")
+def näytä_saldo():
+    saldo = laske_saldo()
+    if saldo > 0:
+        print(f"Saldo: +{saldo}")
+    elif saldo < 0:
+        print(f"Saldo: {saldo}")
+    else:
+        print("Saldo: 0")
 
 while True:
     print("1. Lisää tapahtuma")
     print("2. Näytä tapahtumat")
-    print("3. Hae tapahtumia")
+    print("3. Näytä saldo")
     print("4. Lopeta")
 
     valinta = input("Syötä valintasi: ")
 
-# Start the main loop
-window.mainloop()
+    if valinta == "1":
+        lisää_tapahtuma()
+    elif valinta == "2":
+        näytä_tapahtumat()
+    elif valinta == "3":
+        näytä_saldo()
+    elif valinta == "4":
+        break
