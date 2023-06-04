f197300871d7e1302db1b0e7c1eaf294e2addcd5
makijulius <julle.maki@hotmail.com>
2023-06-04 11:04:16 +0300

    korjauksia

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import csv
 
 tapahtumat = []
 
+#Ohjelman ensimmäinen ja toiminnalle tärkein toiminto on lisätä maksutapahtumia. Append-toiminto tallentaa jokaisen lisätyn tiedon lokaaliin tiedostoon.
 def lisää_tapahtuma():
     nimi = input("Syötä tapahtuman nimi: ")
-    päivämäärä = int(input("Syötä tapahtuman päivämäärä (PP.KK.VVVV): "))
+    päivämäärä = input("Syötä tapahtuman päivämäärä (PP.KK.VVVV): ")
     määrä = float(input("Syötä tapahtuman määrä: "))
     tapahtumat.append({"nimi": nimi, "päivämäärä": päivämäärä, "määrä": määrä})
 
+#Taustalla ohjelmassa lasketaan käyttäjän saldoa, eli kaikkien maksutapahtumien summa tai erotus
 def laske_saldo():
     saldo = 0
     for tapahtuma in tapahtumat:
         saldo += tapahtuma["määrä"]
     return saldo
 
+#Toinen toiminto listaa kaikki käyttäjän lisäämät maksutapahtumat peräkkäin
 def näytä_tapahtumat():
     for tapahtuma in tapahtumat:
         print(f"{tapahtuma['päivämäärä']} - {tapahtuma['nimi']}: {tapahtuma['määrä']}")
 
+#Kolmas toiminto on näyttää saldo, jonka laske_saldo laskee taustalla
 def näytä_saldo():
     saldo = laske_saldo()
     if saldo > 0:
@@ -27,7 +31,12 @@ def näytä_saldo():
     else:
         print("Saldo: 0")
 
+#Tehdään True-silmukka, joka jatkaa ohjelman suorittamista, kunnes se käsketään lopettamaan esim. break-komennolla
 while True:
+    print("########################################")
+    print("####  Tervetuloa finanssilaskuriin  ####")
+    print("########################################")
+    print("")
     print("1. Lisää tapahtuma")
     print("2. Näytä tapahtumat")
     print("3. Näytä saldo")
