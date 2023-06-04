2461667b489900cdf6eb98942898d7c410bc21dd
Ilmari <ilmari.muhonen@gmail.com>
2023-06-04 10:55:19 +0300

    BackToTheBeginning

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ import csv
 
 tapahtumat = []
 
+#Ohjelman ensimmäinen ja toiminnalle tärkein toiminto on lisätä maksutapahtumia
 def lisää_tapahtuma():
-    while True:
-        try:
-            nimi = input("Syötä tapahtuman nimi: ")
-            päivämäärä = int(input("Syötä tapahtuman päivämäärä (PP-KK-VVVV): "))
-            määrä = float(input("Syötä tapahtuman määrä: "))
-            tapahtumat.append({"nimi": nimi, "päivämäärä": päivämäärä, "määrä": määrä})
-            break
-        except ValueError:
-            print("Virheellinen arvo")
+    nimi = input("Syötä tapahtuman nimi: ")
+    päivämäärä = input("Syötä tapahtuman päivämäärä (PP-KK-VVVV): ")
+    määrä = float(input("Syötä tapahtuman määrä: "))
+    tapahtumat.append({"nimi": nimi, "päivämäärä": päivämäärä, "määrä": määrä})
 
+#Taustalla ohjelmassa lasketaan käyttäjän saldoa, eli kaikkien maksutapahtumien summa tai erotus
 def laske_saldo():
     saldo = 0
     for tapahtuma in tapahtumat:
         saldo += tapahtuma["määrä"]
     return saldo
 
+#Toinen toiminto listaa kaikki käyttäjän lisäämät maksutapahtumat peräkkäin
 def näytä_tapahtumat():
     for tapahtuma in tapahtumat:
         print(f"{tapahtuma['päivämäärä']} - {tapahtuma['nimi']}: {tapahtuma['määrä']}")
 
+#Kolmas toiminto on näyttää saldo, jonka laske_saldo laskee taustalla
 def näytä_saldo():
     saldo = laske_saldo()
     if saldo > 0:
@@ -31,15 +30,16 @@ def näytä_saldo():
         print(f"Saldo: {saldo}")
     else:
         print("Saldo: 0")
-
 while True:
+    print("########################################")
+    print("####  Tervetuloa finanssilaskuriin  ####")
+    print("########################################")
+    print("")
     print("1. Lisää tapahtuma")
     print("2. Näytä tapahtumat")
     print("3. Näytä saldo")
     print("4. Lopeta")
-
-    valinta = input("Syötä valintasi: ")
-
+    valinta = input("Syötä valintasi numero: ")
     if valinta == "1":
         lisää_tapahtuma()
     elif valinta == "2":
